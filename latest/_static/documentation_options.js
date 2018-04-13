d107f7c3cbc6710d813e18e56c649c4e40eaeb4e
David B. Kinder <david.b.kinder@intel.com>
2018-04-13 13:51:00 -0700

    publish new directory structure
    Signed-off-by: David B. Kinder <david.b.kinder@intel.com>

---
+++ latest/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: 'v 0.1-rc2',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
