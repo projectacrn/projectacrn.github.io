dfe51e0444a9b23efafe31aa176d52a0c81065f5
David B. Kinder <david.b.kinder@intel.com>
2018-04-13 14:30:20 -0700

    publish latest
    Signed-off-by: David B. Kinder <david.b.kinder@intel.com>

---
+++ 0.1-rc4/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: 'v 0.1-rc4',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
