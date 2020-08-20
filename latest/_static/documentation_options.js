001a34f3458734315fd12471c7145d624bd82596
David B. Kinder <david.b.kinder@intel.com>
2020-08-20 08:27:31 -0700

    publish latest
    Signed-off-by: David B. Kinder <david.b.kinder@intel.com>

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v 2.1-unstable',
+    VERSION: 'v 2.2-unstable',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
